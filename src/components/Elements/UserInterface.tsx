adc4bde3a931997798c2e96593b7158087b1d490
RatnadwipMitra <127027644+RatnadwipMitra@users.noreply.github.com>
2025-09-06 10:44:36 +0530

    hello

--- src/components/Elements/UserInterface.tsx
+++ src/components/Elements/UserInterface.tsx
@@ -181,7 +181,7 @@ const UserInterface = () => {
 
       {/* Popular Movies */}
       <div style={{ color: "white", marginTop: "40px", marginBottom: "0px" }}>
-        <h3>YOUR MOVIES created by ratna </h3>
+        <h3>YOUR MOVIES</h3>
       </div>
       <div
         className="cards-container"
@@ -266,7 +266,7 @@ const UserInterface = () => {
 
       {/* Top Rated Movies */}
       <div style={{ color: "white", marginTop: "40px", marginBottom: "0px" }}>
-        <h3>Top Rated Movies</h3>
+        <h3>Top Rated Movie</h3>
       </div>
       <div
         className="cards-container"
